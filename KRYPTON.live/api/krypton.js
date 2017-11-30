489f146343e4a65c002b0e4647f60ca1696e62b3
Thomas Lobker <thomas@mediaserve.nl>
2017-11-30 23:58:05 +0100

    Check if NEO address is valid before querying NEO RPC

--- KRYPTON.live/api/krypton.js
+++ KRYPTON.live/api/krypton.js
@@ -8,59 +8,62 @@ module.exports = {
 	query: (restify, type, address) => {
 		console.log('query', type, address);
 
-		// Query the blockchain for the UUID
-		node.getStorage(address).then((result) => {
-			if (typeof result === "string") {
-				const uuid = result;
-				console.log('uuid', uuid);
-				switch (type) {
-					case "uuid":
-						restify.result.send({ "status": "success", "type": type, "address": address, "value": uuid });
-						return true;
-//						return uuid;
-					default:
-						node.getStorage(uuid, "scriptHash").then((result) => {
-							if (typeof result === "string") {
-								const provider = result;
-								console.log('provider', provider);
-								switch (type) {
-									case "provider":
-										restify.result.send({ "status": "success", "type": type, "address": address, "value": provider });
-										return true;
-//										return provider;
-									case "location":
-										node.getStorage(provider).then((result) => {
-											if (typeof result === "string") {
-												const location = result;
-												console.log('location', location);
-												restify.result.send({ "status": "success", "type": type, "address": address, "value": location });
-												return true;
-//												return location;
-											} else {
-												console.log('location not found');
+		if (node.isAddress(address)) {
+			// Query the blockchain for the UUID
+			node.getStorage(address).then((result) => {
+				if (typeof result === "string") {
+					const uuid = result;
+					console.log('uuid', uuid);
+					switch (type) {
+						case "uuid":
+							restify.result.send({ "status": "success", "type": type, "address": address, "value": uuid });
+							return true;
+						default:
+							node.getStorage(uuid, "scriptHash").then((result) => {
+								if (typeof result === "string") {
+									const provider = result;
+									console.log('provider', provider);
+									switch (type) {
+										case "provider":
+											restify.result.send({ "status": "success", "type": type, "address": address, "value": provider });
+											return true;
+										case "location":
+											node.getStorage(provider).then((result) => {
+												if (typeof result === "string") {
+													const location = result;
+													console.log('location', location);
+													restify.result.send({ "status": "success", "type": type, "address": address, "value": location });
+													return true;
+												} else {
+													console.log('location not found');
+													return false;
+												}
+											}).catch(function(error) {
+												console.log(error);
 												return false;
-											}
-										}).catch(function(error) {
-											console.log(error);
-											return false;
-										});
+											});
+									}
+								} else {
+									console.log('provider not found');
+									return false;
 								}
-							} else {
-								console.log('provider not found');
+							}).catch(function(error) {
+								console.log(error);
 								return false;
-							}
-						}).catch(function(error) {
-							console.log(error);
-							return false;
-						});
+							});
+					}
+				} else {
+					console.log('uuid not found');
+					return false;
 				}
-			} else {
-				console.log('uuid not found');
+			}).catch(function(error) {
+				console.log(error);
 				return false;
-			}
-		}).catch(function(error) {
-			console.log(error);
+			});
+		} else {
+			console.log("invalid NEO address");
+			restify.result.send({ "status": "failed", "message": "Invalid NEO address" });
 			return false;
-		});
+		}
 	}
 }
